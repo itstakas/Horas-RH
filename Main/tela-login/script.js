31da44e7fe744b7c3a344011c6405396dff18345
Leonardo Takeshi <takeshileonardo.1@gmail.com>
2025-06-18 16:11:43 -0400

    Tela de login

---
+++ Main/tela-login/script.js
@@ -0,0 +1,16 @@
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // Impede o envio padrão do form
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // usuario e senha de teste
+    const usuarioCorreto = 'admin';
+    const senhaCorreta = '1234';
+
+    if (username === usuarioCorreto && password === senhaCorreta){
+        window.location.href = "sistema-de-horas\index.html" 
+    } else {
+        document.getElementById('erro').innerText = "Usuário ou senha incorretos!"
+    }
+})
